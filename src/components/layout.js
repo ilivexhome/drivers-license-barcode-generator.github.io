f3f31763e3a0b8cbb79145d8a0ab81c149258328
Kyle Decot <kyle@joinroot.com>
2018-11-26 15:38:34 -0500

    Deploy

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import Header from './header'
 import Barcode from './barcode'
 
 import { StaticQuery, graphql } from 'gatsby'
