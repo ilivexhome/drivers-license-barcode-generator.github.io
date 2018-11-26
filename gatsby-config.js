a3f99db488fcb009809ed35a29a6c6fa518043f9
Kyle Decot <kyle@joinroot.com>
2018-11-26 11:08:36 -0500

    Adds Eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  pathPrefix: "/drivers-license-barcode-generator",
+  pathPrefix: '/drivers-license-barcode-generator',
   siteMetadata: {
-    title: 'Drivers License Barcode Generator',
+    title: 'Drivers License Barcode Generator'
   },
   plugins: [
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-        includePaths: [],
-      },
+        includePaths: []
+      }
     },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
@@ -29,11 +29,11 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
+  ]
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,48 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import './layout.scss'
+import PDF417 from '../pdf417'
+
+var hub3_code = 'HRVHUB30\nHRK\n' +
+         '000000000012355\n' +
+         'ZELJKO SENEKOVIC\n' +
+         'IVANECKA ULICA 125\n' +
+         '42000 VARAZDIN\n' +
+         '2DBK d.d.\n' +
+         'ALKARSKI PROLAZ 13B\n' +
+         '21230 SINJ\n' +
+         'HR1210010051863000160\n' +
+         'HR01\n' +
+         '7269-68949637676-00019\n' +
+         'COST\n' +
+         'Troskovi za 1. mjesec\n'
+var textToEncode = document.getElementById('textToEncode')
+textToEncode.value = hub3_code
+PDF417.init(hub3_code)
+var barcode = PDF417.getBarcodeArray()
+// block sizes (width and height) in pixels
+var bw = 2
+var bh = 2
+// create canvas element based on number of columns and rows in barcode
+var canvas = document.createElement('canvas')
+canvas.width = bw * barcode['num_cols']
+canvas.height = bh * barcode['num_rows']
+document.getElementById('barcode').appendChild(canvas)
+var ctx = canvas.getContext('2d')
+// graph barcode elements
+var y = 0
+// for each row
+for (var r = 0; r < barcode['num_rows']; ++r) {
+  var x = 0
+  // for each column
+  for (var c = 0; c < barcode['num_cols']; ++c) {
+    if (barcode['bcode'][r][c] == 1) {
+      ctx.fillRect(x, y, bw, bh)
+    }
+    x += bw
+  }
+  y += bh
+}
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -23,7 +65,7 @@ const Layout = ({ children }) => (
           title={data.site.siteMetadata.title}
           meta={[
             { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'keywords', content: 'sample, something' }
           ]}
         >
           <html lang="en" />
@@ -34,7 +76,7 @@ const Layout = ({ children }) => (
             margin: '0 auto',
             maxWidth: 960,
             padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           {children}
@@ -45,7 +87,7 @@ const Layout = ({ children }) => (
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
