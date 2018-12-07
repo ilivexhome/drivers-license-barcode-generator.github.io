86d3790d71b659c45d3b9e5ca3e5d581544dd865
Kyle Decot <kyle@joinroot.com>
2018-12-07 09:51:22 -0500

    Adds Hot Reloading to Docker

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ RUN yarn install
 
 EXPOSE 8000
 
-CMD ["yarn, "start"]
+CMD ["yarn", "start"]
--- src/components/height-input.js
+++ src/components/height-input.js
@@ -8,9 +8,23 @@ export default class HeightInput extends Component {
     unitOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0,
+      rawValue: 0,
+      unit: 'in'
+    }
+  }
+
+  _handleChange = (value) => {
+    // TODO
+  }
+
   _renderUnitOptions() {
     return (
-      <select onChange={this._handleChange}>
+      <select onChange={this._handleUnitChange}>
         {this.props.unitOptions.map((option, index) => {
           return (
             <option key={index}>{option}</option>
@@ -24,7 +38,7 @@ export default class HeightInput extends Component {
     return (
       <div>
         <label>Height</label>
-        <input type="text" />
+        <input onChange={this._handleChange} type="text" />
 
         {this._renderUnitOptions()}
       </div>
